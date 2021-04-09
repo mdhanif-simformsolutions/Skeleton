84bbc7d9073b239664d82c009d43a0e505f99406
Md Hanif <mohammed.h@simformsolutions.com>
2021-04-09 11:26:23 +0530

    Migrated to androidX

--- sample/src/main/java/com/ethanhua/skeleton/sample/adapter/SimpleRcvViewHolder.java
+++ sample/src/main/java/com/ethanhua/skeleton/sample/adapter/SimpleRcvViewHolder.java
@@ -1,6 +1,6 @@
 package com.ethanhua.skeleton.sample.adapter;
 
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.util.SparseArray;
 import android.view.View;
 
